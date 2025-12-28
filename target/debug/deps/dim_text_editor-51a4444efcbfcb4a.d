76868e082aca77d30eb4b739b535cf3db34a3d9c
midlajmarutha <midlajmarutha4@gmail.com>
2025-12-28 22:07:14 +0530

    Seperate files

--- target/debug/deps/dim_text_editor-51a4444efcbfcb4a.d
+++ target/debug/deps/dim_text_editor-51a4444efcbfcb4a.d
@@ -1,5 +1,6 @@
-/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/deps/dim_text_editor-51a4444efcbfcb4a.d: src/main.rs
+/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/deps/dim_text_editor-51a4444efcbfcb4a.d: src/main.rs src/editor.rs
 
-/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/deps/dim_text_editor-51a4444efcbfcb4a: src/main.rs
+/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/deps/dim_text_editor-51a4444efcbfcb4a: src/main.rs src/editor.rs
 
 src/main.rs:
+src/editor.rs:
--- target/debug/dim-text-editor.d
+++ target/debug/dim-text-editor.d
@@ -1,1 +1,1 @@
-/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/dim-text-editor: /home/midlaj/Projects/rust/learing_rust/dim-text-editor/src/main.rs
+/home/midlaj/Projects/rust/learing_rust/dim-text-editor/target/debug/dim-text-editor: /home/midlaj/Projects/rust/learing_rust/dim-text-editor/src/editor.rs /home/midlaj/Projects/rust/learing_rust/dim-text-editor/src/main.rs
