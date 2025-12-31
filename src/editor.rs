c39f75b0fa934c8e5cf229f1aed43b87b86df28c
midlajmarutha <midlajmarutha4@gmail.com>
2026-01-01 00:09:10 +0530

    Tildes

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,7 @@
 use crossterm::event::{Event::Key, read, Event, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::execute;
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode, Clear, ClearType};
-use std::io::stdout;
+mod terminal;
+
+use terminal::Terminal;
 
 pub struct Editor {
     should_quit : bool
@@ -15,53 +15,31 @@ impl Editor {
     }
 
     pub fn run(&mut self){
-        Self::initialize().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let height = Terminal::size()?.1;
+        for current_row in 0..height {
+            print!("~");
+            if current_row + 1 < height {
+                print!("\r\n");
+            }
+        }
+        Ok(())
     }
 
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
-    }
     fn repl(&mut self) -> Result<(),std::io::Error> {
-//        enable_raw_mode()?;
-//        
-//        loop {
-//            if let Key(KeyEvent{code, modifiers, kind, state}) = read()? {
-//                println!("Code: {code:?} Modifiers:{modifiers:?} Kind: {kind:?} State: {state:?} \r");
-//
-//                match code {
-//                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-//                        self.should_quit = true;
-//                    }
-//                    _=>()
-//                }
-//            }
-//            if self.should_quit {
-//                break;
-//            }
-//        }
-//        disable_raw_mode()?;
-//        Ok(())
-
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(())
     }
@@ -81,8 +59,11 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(),std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             println!("Goodbye!!\r");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
         }
         Ok(())
     }
